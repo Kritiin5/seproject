9d7d71807372bda1dfc8d8a09e47606587f5b9f6
ryandcunha25 <ryandcunha2812@gmail.com>
2024-01-15 15:43:47 +0530

    techical changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bookmyshow/main.dart';
+import 'package:seproject/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
