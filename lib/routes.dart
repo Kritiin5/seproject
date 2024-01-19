3441f43d6759d0152018acf5a4ea74abfd892742
ryandcunha25 <ryandcunha2812@gmail.com>
2024-01-19 22:40:25 +0530

    added splash & reset pages

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 class Routes {
-  static String loginRoute = "/login";
-  static String homeRoute = "/home";
+  static String splashScreen = "/";
+  static String loginPage = "/login";
+  static String homePage = "/home";
   static String signUp = "/signup";
-  static String verifyAcc = "/verificaton";
+  static String verifyAccount = "/verification";
+  static String resetPassword = "/reset_password";
 }
