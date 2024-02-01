5007d1d92b137612e7ad4ac95343ed6556a67e47
ryandcunha25 <ryandcunha2812@gmail.com>
2024-02-01 22:19:41 +0530

    no changes

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -12,7 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 5), () {
-      Navigator.pushReplacementNamed(context, Routes.signUp);
+      Navigator.pushReplacementNamed(context, Routes.agreement);
     });
   }
 
