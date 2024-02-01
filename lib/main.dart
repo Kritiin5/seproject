db40fc99ec4f0cf37713243f875007403a0418a4
ryandcunha25 <ryandcunha2812@gmail.com>
2024-02-01 22:20:50 +0530

    added home & dept pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:seproject/departments.dart';
 import 'package:seproject/reset_password.dart';
 import 'package:seproject/splash_screen.dart';
+import 'package:seproject/user_agreement.dart';
 import 'package:seproject/verification_page.dart';
 import 'login_page.dart';
 import 'home_page.dart';
@@ -23,13 +25,16 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routes: {
-        // "/" : (context) => VerificationPage(),
+        // "/" : (context) => Departments(),
         Routes.splashScreen: (context) => SplashScreen(),
+        Routes.agreement : (context) => UserAgreementPage(),
         Routes.signUp: (context) => SignUpPage(),
         Routes.loginPage: (context) => LoginPage(),
-        Routes.homePage: (context) => HomePage(),
         Routes.verifyAccount: (context) => VerificationPage(),
         Routes.resetPassword: (context) => ResetPasswordPage(),
+        Routes.homePage: (context) => HomePage(),
+        Routes.departments : (context) => Departments(),
+        
       },
       debugShowCheckedModeBanner: false,
     );
